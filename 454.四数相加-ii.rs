f244bf08725056907c0466a69693441fd311c57c
Baoyx007 <baoyx007@gamil.com>
2019-06-08 16:14:25 +0800

    feat: 454

--- 454.四数相加-ii.rs
+++ 454.四数相加-ii.rs
@@ -1,5 +1,3 @@
-struct Solution;
-
 use std::collections::HashMap;
 
 impl Solution {
@@ -26,22 +24,4 @@ impl Solution {
 
     ret
   }
-}
-
-#[test]
-fn case() {
-  assert_eq!(
-    Solution::search_range(vec![5, 7, 7, 8, 8, 10], 8),
-    vec![3, 4]
-  );
-  assert_eq!(
-    Solution::search_range(vec![5, 7, 7, 8, 8, 10], 6),
-    vec![-1, -1]
-  );
-  assert_eq!(Solution::search_range(vec![1], 0), vec![-1, -1]);
-}
-
-fn main() {
-  let obj = Solution::four_sum_count(vec![1, 2], vec![-2, -1], vec![-1, 2], vec![0, 2]);
-  // println!("{:?}", Solution::search_range(vec![1, 4], 4));
 }
