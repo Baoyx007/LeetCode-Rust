f1744b09130cd6a4a4a771c0f7bd6cc372587826
Baoyx007 <baoyx007@gmail.com>
2020-03-25 13:32:57 +0800

    892

--- 892.三维形体的表面积.rs
+++ 892.三维形体的表面积.rs
@@ -1,6 +1,8 @@
-struct Solution;
-
-use leetcode::vec2d;
+/*
+ * @lc app=leetcode.cn id=892 lang=rust
+ *
+ * [892] 三维形体的表面积
+ */
 
 // @lc code=start
 impl Solution {
@@ -12,28 +14,16 @@ impl Solution {
 
         for (idx, row) in grid.iter().enumerate() {
             for (idy, &cell) in row.iter().enumerate() {
-                dbg!((idx, idy));
-
                 if cell > 0 {
                     ret += 2;
 
                     for i in 0..4 {
-                        // dx[i]
-                        // let target_value = grid
-                        //     .get(dx[i])
-                        //     .and_then(|lastRow| lastRow.get(dy[i]))
-                        //     .unwrap_or(&0);
-
-                        // dbg!(target_value);
-                        // grid.get(1i32.into());
-
                         let (x, y): (i32, i32) = (idx as i32 + dx[i], idy as i32 + dy[i]);
                         let mut target = 0;
                         if x >= 0 && x < grid.len() as i32 && y >= 0 && y < row.len() as i32 {
                             target = grid[x as usize][y as usize];
                         }
                         ret += std::cmp::max(cell - target, 0);
-                        dbg!(ret);
                     }
                 }
             }
@@ -43,10 +33,3 @@ impl Solution {
     }
 }
 // @lc code=end
-
-fn main() {
-    println!(
-        "{:?}",
-        Solution::surface_area(vec2d![[2, 2, 2], [2, 1, 2], [2, 2, 2]])
-    );
-}
