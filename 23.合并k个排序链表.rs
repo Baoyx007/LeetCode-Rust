6de2ace3d1cee6ccfe6e658df10c06131f40d4d8
Baoyx007 <baoyx007@gmail.com>
2020-04-26 22:20:06 +0800

    fuck linked list

--- 23.合并k个排序链表.rs
+++ 23.合并k个排序链表.rs
@@ -2,24 +2,24 @@ struct Solution;
 
 #[derive(PartialEq, Eq, Clone, Debug)]
 pub struct ListNode {
-  pub val: i32,
-  pub next: Option<Box<ListNode>>,
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
 }
 
 impl ListNode {
-  #[inline]
-  fn new(val: i32) -> Self {
-    ListNode { next: None, val }
-  }
+    #[inline]
+    fn new(val: i32) -> Self {
+        ListNode { next: None, val }
+    }
 }
 
 impl std::fmt::Display for ListNode {
-  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-    match &self.next {
-      None => write!(f, "{}", self.val),
-      Some(next) => write!(f, "{} -> {}", self.val, next),
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match &self.next {
+            None => write!(f, "{}", self.val),
+            Some(next) => write!(f, "{} -> {}", self.val, next),
+        }
     }
-  }
 }
 
 // macro_rules! linked {
@@ -38,82 +38,110 @@ use core::cmp::Ordering;
 use std::collections::BinaryHeap;
 
 impl Ord for ListNode {
-  fn cmp(&self, other: &Self) -> Ordering {
-    self.val.cmp(&other.val).reverse()
-  }
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.val.cmp(&other.val).reverse()
+    }
 }
 
 impl PartialOrd for ListNode {
-  fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-    Some(self.cmp(other))
-  }
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
 }
 
 impl Solution {
-  pub fn merge_k_lists(lists: Vec<Option<Box<ListNode>>>) -> Option<Box<ListNode>> {
-    let head = Box::new(ListNode::new(-1));
-    let mut priority_queue = BinaryHeap::with_capacity(lists.len());
+    pub fn merge_k_lists(lists: Vec<Option<Box<ListNode>>>) -> Option<Box<ListNode>> {
+        let head = Box::new(ListNode::new(-1));
+        let mut priority_queue = BinaryHeap::with_capacity(lists.len());
 
-    for vec in lists {
-      if let Some(node) = vec {
-        priority_queue.push(node);
-      }
+        for vec in lists {
+            if let Some(node) = vec {
+                priority_queue.push(node);
+            }
+        }
+
+        println!("{:?}", &priority_queue);
+
+        let mut tmp = &head;
+        while let Some(node) = priority_queue.pop() {
+            println!("{:?}", &head.next);
+            tmp.next = Some(node);
+            tmp = &tmp.next.unwrap();
+            if let Some(next) = tmp.next {
+                priority_queue.push(next);
+                tmp.next = None;
+            }
+        }
+
+        head.next
     }
-
-    println!("{:?}", &priority_queue);
-
-    let mut tmp = &head;
-    while let Some(node) = priority_queue.pop() {
-      println!("{:?}", &head.next);
-      tmp.next = Some(node);
-      tmp = &tmp.next.unwrap();
-      if let Some(next) = tmp.next {
-        priority_queue.push(next);
-        tmp.next = None;
-      }
-    }
-
-    head.next
-  }
 }
 
 #[test]
 fn case() {
-  assert_eq!(
-    Solution::search_range(vec![5, 7, 7, 8, 8, 10], 8),
-    vec![3, 4]
-  );
-  assert_eq!(
-    Solution::search_range(vec![5, 7, 7, 8, 8, 10], 6),
-    vec![-1, -1]
-  );
-  assert_eq!(Solution::search_range(vec![1], 0), vec![-1, -1]);
+    assert_eq!(
+        Solution::search_range(vec![5, 7, 7, 8, 8, 10], 8),
+        vec![3, 4]
+    );
+    assert_eq!(
+        Solution::search_range(vec![5, 7, 7, 8, 8, 10], 6),
+        vec![-1, -1]
+    );
+    assert_eq!(Solution::search_range(vec![1], 0), vec![-1, -1]);
 }
 
 fn main() {
-  let h1 = {
-    let mut head = Box::new(ListNode::new(1));
-    let mut l2 = Box::new(ListNode::new(4));
-    l2.next = Some(Box::new(ListNode::new(5)));
-    head.next = Some(l2);
-    Some(head)
-  };
+    let h1 = {
+        let mut head = Box::new(ListNode::new(1));
+        let mut l2 = Box::new(ListNode::new(4));
+        l2.next = Some(Box::new(ListNode::new(5)));
+        head.next = Some(l2);
+        Some(head)
+    };
 
-  let h2 = {
-    let mut head = Box::new(ListNode::new(1));
-    let mut l2 = Box::new(ListNode::new(3));
-    l2.next = Some(Box::new(ListNode::new(4)));
-    head.next = Some(l2);
-    Some(head)
-  };
+    let h2 = {
+        let mut head = Box::new(ListNode::new(1));
+        let mut l2 = Box::new(ListNode::new(3));
+        l2.next = Some(Box::new(ListNode::new(4)));
+        head.next = Some(l2);
+        Some(head)
+    };
 
-  let h3 = {
-    let mut head = Box::new(ListNode::new(2));
-    let mut l2 = Box::new(ListNode::new(6));
-    head.next = Some(l2);
-    Some(head)
-  };
+    let h3 = {
+        let mut head = Box::new(ListNode::new(2));
+        let mut l2 = Box::new(ListNode::new(6));
+        head.next = Some(l2);
+        Some(head)
+    };
 
-  let ret = Solution::merge_k_lists(vec![h1, h2, h3]);
-  println!("{:?}", ret);
+    let ret = Solution::merge_k_lists(vec![h1, h2, h3]);
+    println!("{:?}", ret);
+}
+
+impl Solution {
+    pub fn merge_k_lists(lists: Vec<Option<Box<ListNode>>>) -> Option<Box<ListNode>> {
+        let mut ret_head = Box::new(ListNode::new(-1));
+
+        loop {
+            let mut min = -1;
+            let mut min_ptr: &Option<Box<ListNode>> = &None;
+            for l in &lists {
+                if let Some(node) = l {
+                    if node.val < min {
+                        min = node.val;
+                        min_ptr = l;
+                    }
+                }
+            }
+
+            if let Some(node) = min_ptr {
+                ret_head.next = Some(node.clone());
+                ret_head = ret_head.next.unwrap();
+
+                min_ptr = node.next;
+            }
+        }
+
+        None
+    }
 }
--- main.rs
+++ main.rs
@@ -1,38 +1,79 @@
 struct Solution;
 
 /*
- * @lc app=leetcode.cn id=46 lang=rust
+ * @lc app=leetcode.cn id=23 lang=rust
  *
- * [46] 全排列
+ * 23. 合并K个排序链表
  */
 
-// @lc code=start
-impl Solution {
-    fn bt(nums: &Vec<i32>, ret: &mut Vec<Vec<i32>>, track: &mut Vec<i32>) {
-        if track.len() == nums.len() {
-            ret.push(track.clone());
-            return;
-        }
-        for n in nums {
-            if track.contains(&n) {
-                continue;
-            }
-            track.push(*n);
-            Self::bt(nums, ret, track);
-            track.pop();
-        }
+// Definition for singly-linked list.
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
+}
+
+impl ListNode {
+    #[inline]
+    fn new(val: i32) -> Self {
+        ListNode { next: None, val }
     }
+}
+// @lc code=start
 
-    pub fn permute(nums: Vec<i32>) -> Vec<Vec<i32>> {
-        let mut ret: Vec<Vec<i32>> = Vec::new();
+impl Solution {
+    pub fn merge_k_lists(lists: Vec<Option<Box<ListNode>>>) -> Option<Box<ListNode>> {
+        let mut ret_head = Box::new(ListNode::new(-1));
 
-        Self::bt(&nums, &mut ret, &mut vec![]);
-        ret
+        loop {
+            let mut min = -1;
+            let mut min_ptr: &Option<Box<ListNode>> = &None;
+            for l in &lists {
+                if let Some(node) = l {
+                    if node.val < min {
+                        min = node.val;
+                        min_ptr = l;
+                    }
+                }
+            }
+
+            if let Some(node) = min_ptr {
+                ret_head.next = Some(node.clone());
+                ret_head = ret_head.next.unwrap();
+
+                min_ptr = node.next;
+            }
+        }
+
+        None
     }
 }
 // @lc code=end
 
 fn main() {
-    println!("{:?}", Solution::permute(vec![1, 2, 3]));
-    // assert_eq!("",)
+    let h1 = {
+        let mut head = Box::new(ListNode::new(1));
+        let mut l2 = Box::new(ListNode::new(4));
+        l2.next = Some(Box::new(ListNode::new(5)));
+        head.next = Some(l2);
+        Some(head)
+    };
+
+    let h2 = {
+        let mut head = Box::new(ListNode::new(1));
+        let mut l2 = Box::new(ListNode::new(3));
+        l2.next = Some(Box::new(ListNode::new(4)));
+        head.next = Some(l2);
+        Some(head)
+    };
+
+    let h3 = {
+        let mut head = Box::new(ListNode::new(2));
+        let mut l2 = Box::new(ListNode::new(6));
+        head.next = Some(l2);
+        Some(head)
+    };
+
+    let ret = Solution::merge_k_lists(vec![h1, h2, h3]);
+    println!("{:?}", ret);
 }
